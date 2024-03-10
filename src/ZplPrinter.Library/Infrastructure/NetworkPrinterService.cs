d7a657ef10b30c2e55bfa38bcc6af902833e5420
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-03-10 12:40:10 +0100

    Dodanie drukarki sieciowej

---
+++ src/ZplPrinter.Library/Infrastructure/NetworkPrinterService.cs
@@ -0,0 +1,29 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using ZplPrinter.Library.Abstractions;
+
+namespace ZplPrinter.Library.Infrastructure;
+
+public class NetworkPrinterService : IPrinterService
+{
+    private IPAddress ipAddress;
+    private int port;
+
+    public NetworkPrinterService(IPAddress ipAddress, int port)
+    {
+        this.ipAddress = ipAddress;
+        this.port = port;
+    }
+
+    public void Print(string content)
+    {
+        TcpClient tcpClient = new TcpClient();
+        tcpClient.Connect(ipAddress, port);
+
+        var stream = new StreamWriter(tcpClient.GetStream());
+        stream.Write(content);
+        stream.Flush();
+        stream.Close();
+        tcpClient.Close();
+    }
+}
