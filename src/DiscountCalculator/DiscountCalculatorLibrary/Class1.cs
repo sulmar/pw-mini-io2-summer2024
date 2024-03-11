dd27a6e55a79ea28c8a3e2b7aa793d8409638ba4
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-03-11 15:52:44 +0100

    Utworzenie klasy kalkulatora rabatów

--- src/DiscountCalculator/DiscountCalculatorLibrary/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace DiscountCalculatorLibrary;
-
-public class Class1
-{
-
-}
---
+++ src/DiscountCalculator/DiscountCalculatorLibrary/DiscountCalculator.cs
@@ -0,0 +1,9 @@
+﻿namespace DiscountCalculatorLibrary;
+
+public class DiscountCalculator
+{
+    public decimal CalculateDiscount(decimal price, string discountCode)
+    {
+        throw new NotImplementedException();
+    }
+}
