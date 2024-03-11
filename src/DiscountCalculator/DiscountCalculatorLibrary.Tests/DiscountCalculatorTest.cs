058f86a908556df55d2e8d29b543d602a5cd6d44
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-03-11 16:09:09 +0100

    Refaktor testów

--- src/DiscountCalculator/DiscountCalculatorLibrary.Tests/DiscountCalculatorTest.cs
+++ src/DiscountCalculator/DiscountCalculatorLibrary.Tests/DiscountCalculatorTest.cs
@@ -2,11 +2,16 @@ namespace DiscountCalculatorLibrary.Tests;
 
 public class DiscountCalculatorTest
 {
+    private readonly DiscountCalculator discountCalculator;
+
+    public DiscountCalculatorTest()
+    {
+        discountCalculator = new DiscountCalculator();
+    }
+
     [Fact]
     public void CalculateDiscount_EmptyDiscountCode_ShouldBeOriginalPrice()
     {
-        DiscountCalculator discountCalculator = new DiscountCalculator();
-
         var result = discountCalculator.CalculateDiscount(1, string.Empty);
 
         Assert.Equal(1, result);
@@ -15,8 +20,6 @@ public class DiscountCalculatorTest
     [Fact]
     public void CalculateDiscount_SAVE10NOWDiscountCode_ShouldBeDiscountedBy10PercentPrice()
     {
-        DiscountCalculator discountCalculator = new DiscountCalculator();
-
         var result = discountCalculator.CalculateDiscount(10, "SAVE10NOW");
 
         Assert.Equal(9, result);
@@ -25,8 +28,6 @@ public class DiscountCalculatorTest
     [Fact]
     public void CalculateDiscount_DISCOUNT20OFFDiscountCode_ShouldBeDiscountedBy20PercentPrice()
     {
-        DiscountCalculator discountCalculator = new DiscountCalculator();
-
         var result = discountCalculator.CalculateDiscount(10, "DISCOUNT20OFF");
 
         Assert.Equal(8, result);
@@ -35,8 +36,6 @@ public class DiscountCalculatorTest
     [Fact]
     public void CalculateDiscount_PriceBelowZero_ShouldThrowArgumentExceptionWithMessage()
     {
-        DiscountCalculator discountCalculator = new DiscountCalculator();
-
         Action act = () => discountCalculator.CalculateDiscount(-1, string.Empty);
 
         var exception = Assert.Throws<ArgumentException>(act);
@@ -47,7 +46,6 @@ public class DiscountCalculatorTest
     public void CalculateDiscount_InvalidDiscountCode_ShouldThrowArgumentExceptionWithMessage()
     {
         const string InvalidDiscountCode = "A";
-        DiscountCalculator discountCalculator = new DiscountCalculator();
 
         Action act = () => discountCalculator.CalculateDiscount(0, InvalidDiscountCode);
 
