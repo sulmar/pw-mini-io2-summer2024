a78f6276ee8791079013ee8a2e980934b70609a5
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-03-18 17:49:28 +0100

    refactor: Utworzenie abstrakcyjnej fabryki

--- src/DiscountCalculator/DiscountCalculatorLibrary/DiscountStrategyFactory.cs
+++ src/DiscountCalculator/DiscountCalculatorLibrary/DiscountStrategyFactory.cs
@@ -1,27 +1,27 @@
-﻿namespace DiscountCalculatorLibrary
+﻿namespace DiscountCalculatorLibrary;
+
+
+// Concrete Factory
+public class DiscountStrategyFactory : IDiscountStrategyFactory
 {
-    public class DiscountStrategyFactory
-    {
-        private readonly HashSet<string> discountCodes = [];
+    private readonly HashSet<string> discountCodes = [];
 
-        public DiscountStrategyFactory()
-        {
-            
-        }
-
-        public DiscountStrategyFactory(IEnumerable<string> discountCodes)
-            : this()
-        {
-            this.discountCodes = discountCodes.ToHashSet();
-        }
-
-        public IDiscountStrategy Create(string discountCode) => discountCode switch
-        {
-            "SAVE10NOW" => new PercentageDiscountStrategy(0.1m),
-            "DISCOUNT20OFF" => new PercentageDiscountStrategy(0.2m),
-            var _ when discountCodes.Remove(discountCode)
-                => new PercentageDiscountStrategy(0.5m),
-            _ => throw new ArgumentException("Invalid discount code")
-        };
+    public DiscountStrategyFactory()
+    {        
     }
+
+    public DiscountStrategyFactory(IEnumerable<string> discountCodes)
+        : this()
+    {
+        this.discountCodes = discountCodes.ToHashSet();
+    }
+
+    public IDiscountStrategy Create(string discountCode) => discountCode switch
+    {
+        "SAVE10NOW" => new PercentageDiscountStrategy(0.1m),
+        "DISCOUNT20OFF" => new PercentageDiscountStrategy(0.2m),
+        var _ when discountCodes.Remove(discountCode)
+            => new PercentageDiscountStrategy(0.5m),
+        _ => throw new ArgumentException("Invalid discount code")
+    };
 }
---
+++ src/DiscountCalculator/DiscountCalculatorLibrary/IDiscountStrategyFactory.cs
@@ -0,0 +1,7 @@
+﻿namespace DiscountCalculatorLibrary;
+
+// Abstract Factory
+public interface IDiscountStrategyFactory
+{
+    IDiscountStrategy Create(string discountCode);
+}
