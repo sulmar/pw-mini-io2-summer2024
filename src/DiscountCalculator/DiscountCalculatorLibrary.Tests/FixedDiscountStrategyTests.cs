e7fbcdcfc3abc862a5a6775725f56e777828f0e0
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-03-18 17:50:00 +0100

    Utworzenie testów strategii

---
+++ src/DiscountCalculator/DiscountCalculatorLibrary.Tests/FixedDiscountStrategyTests.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics.SymbolStore;
+
+namespace DiscountCalculatorLibrary.Tests;
+
+public class FixedDiscountStrategyTests
+{
+    [Theory]
+    [InlineData(10, 1, 1)]
+    [InlineData(10, 2, 2)]
+    public void CalculateDiscount_WhenCalled_ShouldBeDiscounted(decimal price, decimal amount, decimal expectedPrice)
+    {
+        IDiscountStrategy sut = new FixedDiscountStrategy(amount);
+
+        var result = sut.CalculateDiscount(price);
+
+        Assert.Equal(expectedPrice, result);
+    }
+}
---
+++ src/DiscountCalculator/DiscountCalculatorLibrary.Tests/PercentageDiscountStrategyTests.cs
@@ -0,0 +1,16 @@
+﻿namespace DiscountCalculatorLibrary.Tests;
+
+public class PercentageDiscountStrategyTests
+{
+    [Theory]
+    [InlineData(10, 0.1, 1)]
+    [InlineData(10, 0.2, 2)]
+    public void CalculateDiscount_WhenCalled_ShouldBeDiscounted(decimal price, decimal percentage, decimal expectedPrice)
+    {
+        IDiscountStrategy sut = new PercentageDiscountStrategy(percentage);
+
+        var result = sut.CalculateDiscount(price);
+
+        Assert.Equal(expectedPrice, result);
+    }
+}
