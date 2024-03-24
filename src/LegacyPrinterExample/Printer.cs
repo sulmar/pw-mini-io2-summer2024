b692d0593f1f6568070536dc01cdc18f53e71ea9
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-03-24 20:02:34 +0100

    Dodanie starej drukarki

--- src/LegacyPrinterExample/Printer.cs
+++ src/LegacyPrinterExample/Printer.cs
@@ -2,5 +2,33 @@ namespace LegacyPrinterExample;
 
 public class Printer
 {
+    decimal _costPerCopy = 0.1m; // Cost of 0.10 zł per copy
+    public int Counter { get; private set; }
     
+    public void Print(string document, int copies = 1)
+    {
+        for (int copy = 1; copy <= copies; copy++)
+        {
+            Console.WriteLine($"Printer is printing: {document}");
+        }
+
+        var cost = CalculateCost(copies);
+        Console.WriteLine($"{cost:C2}");
+        
+        Counter += copies;
+    }
+
+    private decimal CalculateCost(int copies)
+    {
+        return copies * _costPerCopy;
+    }
+}
+
+// Uwaga: Nie zmieniaj kodu tej klasy!
+public sealed class LegacyPrinter
+{
+    public void PrintDocument(string document)
+    {
+        Console.WriteLine($"Legacy Printer is printing: {document}");
+    }
 }
