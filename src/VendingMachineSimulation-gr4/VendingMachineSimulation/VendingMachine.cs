fbb08f803d7cd1f0e791a7789237130e1e6bba8d
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-04-08 15:38:36 +0200

    Utworzenie projektu

---
+++ src/VendingMachineSimulation-gr4/VendingMachineSimulation/VendingMachine.cs
@@ -0,0 +1,86 @@
+﻿namespace VendingMachineSimulation
+{
+    public interface IProduct
+    {
+        string Name { get; }
+        decimal Price { get; }
+    }
+
+    public class BasicProduct : IProduct
+    {
+        public string Name { get; }
+        public decimal Price { get; }
+    }
+
+    public enum PaymentMethod
+    {
+        Card,
+        Cash,
+        Blik
+    }
+
+    public enum State
+    {
+        Idle,
+        Selecting,
+        Checkout,
+        AwaitingPayment
+    }
+
+    public class VendingMachine
+    {
+        public VendingMachine()
+        {
+            State = State.Idle;
+        }
+
+        public State State { get; private set; }
+
+        private List<IProduct> selectedProducts = new List<IProduct>();
+
+        public void SelectProduct(IProduct product)
+        {
+            if (State != State.Idle && State != State.Selecting)
+                throw new InvalidOperationException();
+            
+            selectedProducts.Add(product);
+
+            State = State.Selecting;
+        }
+
+        public void ConfirmSelected()
+        {
+            if (State != State.Selecting)
+                throw new InvalidOperationException();
+
+            State = State.Checkout;
+        }
+
+        public void DeleteProduct(IProduct product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ClearSelected()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Pay(PaymentMethod method)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ConfirmPayment()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CancelPayment()
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
